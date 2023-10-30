58c02505c7b17be89578b2dac10c14abdb74d797
Suraj Yadav <90672206+suraj-yadav0@users.noreply.github.com>
2023-10-30 19:32:35 +0530

    final commit

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -10,15 +10,52 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
   LoginController controller = Get.put(LoginController());
+  @override
+  void initState() {
+    
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.blue,
         title: const Text("GETx Tutorial"),
       ),
-      body: const Column(children: []),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              TextFormField(
+                controller: controller.emailControler.value,
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(), hintText: 'Email'),
+              ),
+              const SizedBox(height: 20,),
+              TextFormField(
+                controller: controller.passwordControler.value,
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(), hintText: 'Password'),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              InkWell(
+                onTap: () {
+                  controller.loginapi();
+                },
+                child: Container(
+                  height: 45,
+                  width: double.infinity,
+                  color: Colors.blue,
+                  child: const Center(child:  Text('LogIn')),
+                ),
+              )
+            ]),
+      ),
     );
   }
 }
--- lib/login_controller.dart
+++ lib/login_controller.dart
@@ -1,7 +1,36 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:http/http.dart';
 
 class LoginController extends GetxController {
   final emailControler = TextEditingController().obs;
-   final passwordControler = TextEditingController().obs;
+  final passwordControler = TextEditingController().obs;
+
+  void loginapi() async {
+try{
+    final response = await post(Uri.parse('https://reqres.in/api/login'),
+    body: {
+      'email' : emailControler.value.text,
+      'password' : passwordControler.value.text
+    });
+
+    var data = jsonDecode(response.body);
+
+    print(response.statusCode);
+    print(data);
+
+    if(response.statusCode == 200) {
+      
+Get.snackbar('Login Successful', 'Congratulations');
+    }else {
+      Get.snackbar('Login Failed', data['error']);
+    }
+}catch (e){
+Get.snackbar('Exception', e.toString());
+}
+
+    
+  }
 }
